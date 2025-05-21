bec92ff5bd7f62b475be136849ef865bdeafb0c5
Arsh-yar <khanarshyar@gmail.com>
2025-05-21 14:46:32 +0530

    update port

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 console.log("hello world");
 const express = require("express");
 const app = express();
-const port = 5000;
+const port = 6000;
 app.listen(port, () => {
     console.log("server is running at port 3000");
 });
